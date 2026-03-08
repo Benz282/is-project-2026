11bd3cd3cbc5aa02c3173288f03e6e539cd56d28
Benz282 <yosita.appt30@gmail.com>
2026-03-09 01:09:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ if menu == "หน้าแรก":
 elif menu == "ทดสอบโมเดล 1 (Ice)":
     st.title("📊 การพัฒนาโมเดลทำนายค่าเซนเซอร์ (Ice Skating Data)")
     
-    # --- ส่วนอธิบายเนื้อหา (Documentation) ---
+# --- ส่วนอธิบายเนื้อหา (Documentation) ---
     with st.expander("📖 รายละเอียดแนวทางการพัฒนาโมเดล (คลิกเพื่ออ่าน)", expanded=True):
         st.subheader("1. การเตรียมข้อมูล (Data Preparation)")
         st.write("""
@@ -182,6 +182,7 @@ elif menu == "ทดสอบโมเดล 2 (MNIST)":
                     
                     st.success(f"🎯 AI วิเคราะห์ว่าเป็นเลข: {final_res}")
                     st.write(f"ความเชื่อมั่น: {confidence:.2f}%")
+
 
 
 
